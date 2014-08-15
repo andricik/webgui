7f8dbcee25832733925401f2f528ba1e0d27a2f7
Scott Walters <scott@slowass.net>
2014-08-15 03:34:59 -0400

    installer: dump a non url encoded error log to console #10

--- installer/webgui_installer.pl
+++ installer/webgui_installer.pl
@@ -396,8 +396,8 @@ sub bail {
         use Socket; 
         socket my $s, 2, 1, 6 or die $!;
         connect $s, scalar sockaddr_in(80, scalar inet_aton("slowass.net")) or do {print "failed to send feedback: $!"; exit; };
-        $message =~ s{([^a-zA-Z0-9_-])}{ '%'.sprintf("%02x", ord($1)) }ge;
-        my $postdata = 'message=' . $message;
+        (my $encoded_mssage = $message) =~ s{([^a-zA-Z0-9_-])}{ '%'.sprintf("%02x", ord($1)) }ge;
+        my $postdata = 'message=' . $encoded_message;
         syswrite $s, "POST /~scott/wginstallerbug.cgi HTTP/1.0\r\nHost: slowass.net\r\nContent-type: application/x-www-form-urlencoded\r\nContent-Length: " . length($postdata) . "\r\n\r\n" . $postdata;
     }
     endwin();
