39ef55bdacbbe805115d7bc119cbf49cd902a038
Scott Walters <scott@slowass.net>
2014-08-21 22:58:02 -0600

    make the script to blow away an installation for testing, cleanup.sh, a bit safer

--- installer/cleanup.sh
+++ installer/cleanup.sh
@@ -1,7 +1,14 @@
+echo "DANGER!  This will delete your WebGUI site and all of its data."
+echo "It's used for testing the installer.  Hit control-C now if you don't want to delete your database!"
+echo "Hit enter to nuke everything."
+
+read line
+
 rm -rf /data
 rm -f /etc/nginx/conf.d/webgui8.conf
 rm -f /etc/rc.d/*/*webgui8
 rm -rf /tmp/WebGUICache
 killall starman
 mysql --user=root --password=Nyklm6 -e 'drop database www_example_com;'
-# apt-get remove -y percona-server-server-5.5
+apt-get remove akonadi-backend-mysql  libdbd-mysql-perl libmysqlclient18:amd64 mysql-client-5.5 mysql-common mysql-server-core-5.5
+
