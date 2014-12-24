0ef7115942c5178d3a75262ea2eca2e269342f54
Scott Walters <scott@slowass.net>
2014-12-24 16:55:38 -0500

    Bump the version of MooseX::NonMoose required.  Some previous versions had a bug where attempting to 'extends' a module without a VERSION would blow up, even though no specific version of that module was requested. See 782ee7a1e3f15145ec01a8b7d9413f8bea0ef26c and doy/moosex-nonmoose#12

--- sbin/testEnvironment.pl
+++ sbin/testEnvironment.pl
@@ -153,7 +153,7 @@ checkModule("JavaScript::Minifier::XS",     "0.05"       );
 checkModule("Readonly",                     "1.03"       );
 checkModule("Moose",                        "0.93"       );
 checkModule("MooseX::Storage",              "0.23"       );
-checkModule("MooseX::NonMoose",             '0.07'       );
+checkModule("MooseX::NonMoose",             '0.26'       );
 checkModule("MooseX::Storage::Format::JSON","0.27"       );
 checkModule("namespace::autoclean",         "0.09"       );
 checkModule("Business::PayPal::API",        "0.62"       );
